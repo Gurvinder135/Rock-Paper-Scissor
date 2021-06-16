7138f7cdba7da11572a1b9f8bdedea88461ede0d
Gurvinder <gurvinder135@gmail.com>
2021-06-15 21:49:15 -0500

    added graphical interface

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
+// variable assingment
+let rock= document.querySelector("#rock");
+let paper= document.querySelector("#paper");
+let scissor= document.querySelector("#scissor");
 
+let you_score= document.querySelector("#you_score");
+let comp_score= document.querySelector("#comp_score");
+let comment= document.querySelector("#comment");
+let yourscore=0;
+let compscore=0;
+
+// logic
+rock.addEventListener("click", function(){
+playRound("ROCK",computerPlay());
+})
+paper.addEventListener("click", function(){
+    playRound("PAPER",computerPlay());  
+ })
+scissor.addEventListener("click", function(){
+        playRound("SCISSOR",computerPlay());      
+ })
+
+// Random generator
 function computerPlay(){
-    arrayList=["ROCK", "PAPER", "SEASOR"];
+    arrayList=["ROCK", "PAPER", "SCISSOR"];
     return arrayList[Math.floor(Math.random()*arrayList.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection===computerSelection){
-        return("Game draw");
-    } else if((playerSelection==="ROCK" && computerSelection==="SEASOR")||(playerSelection==="SEASOR"&&computerSelection==="PAPER")||(playerSelection==="PAPER"&& computerSelection==="ROCK")){
-        score=score+1;
-        return(`You Win ${playerSelection} beats ${computerSelection}`);
+        comment.textContent="Game draw";
+    } else if((playerSelection==="ROCK" && computerSelection==="SCISSOR")||(playerSelection==="SCISSOR"&&computerSelection==="PAPER")||(playerSelection==="PAPER"&& computerSelection==="ROCK")){
+        yourscore=yourscore+1;
+        you_score.textContent=yourscore
+        comment.textContent=`You Win ${playerSelection} beats ${computerSelection}`;
     } else {
-        if(score>0){
-            score=score-1;
-        }
-        return(`You Lose ${computerSelection} beats ${playerSelection}`);
+            compscore=compscore+1;
+            comp_score.textContent=compscore;
+        comment.textContent=`You Lose ${computerSelection} beats ${playerSelection}`;
     }
 }
-let numberOfRounds= parseInt(prompt("Welcome to the game, How many rounds you want to play?"));
-while (isNaN(numberOfRounds)){
-    numberOfRounds=parseInt(prompt("Invalid input! Please enter integer value, How many rounds you want to play?"));
-}
-let score=0;
-function game(round){
-    
- for(let i=0; i<round; i++){   
-    let userInput=(prompt("Enter your choice from Rock, Paper, Seasor!")).toUpperCase();
-    while(userInput!=="ROCK"&& userInput!=="PAPER"&& userInput!=="SEASOR"){
-        userInput=(prompt("Invalid entry! Please only enter from from Rock, Paper, Seasor!")).toUpperCase();
-    }
-console.log(playRound(userInput, computerPlay()) + ` and your score is ${score}`);
- }
-}
-
-game(numberOfRounds);
