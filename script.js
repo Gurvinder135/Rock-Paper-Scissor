d5cfcf82ab50681cefe5b4f115aea84c96329584
Gurvinder <gurvinder135@gmail.com>
2021-06-16 19:25:38 -0500

    logic & html updated

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ let comp_score= document.querySelector("#comp_score");
 let comment= document.querySelector("#comment");
 let yourscore=0;
 let compscore=0;
+let reset= document.querySelector(".reset");
+let main= document.querySelector(".main");
+let win= document.querySelector(".win");
+
 
 // logic
 rock.addEventListener("click", function(){
@@ -31,11 +35,23 @@ function playRound(playerSelection, computerSelection) {
         comment.textContent="Game draw";
     } else if((playerSelection==="ROCK" && computerSelection==="SCISSOR")||(playerSelection==="SCISSOR"&&computerSelection==="PAPER")||(playerSelection==="PAPER"&& computerSelection==="ROCK")){
         yourscore=yourscore+1;
+        if(yourscore===5){
+            end("you Win! MANKIND LIVES ANOTHER DAY!!");
+        }
         you_score.textContent=yourscore
         comment.textContent=`You Win ${playerSelection} beats ${computerSelection}`;
     } else {
             compscore=compscore+1;
+            if(compscore===5){
+                end("You Lost...WHO WILL SAVE MANKIND NOW?");
+            }
             comp_score.textContent=compscore;
         comment.textContent=`You Lose ${computerSelection} beats ${playerSelection}`;
     }
+}
+function end(wins){
+    win.textContent=`${wins}`;
+    main.className="main1";
+    reset.className="show";
+    win.className="show";
 }
